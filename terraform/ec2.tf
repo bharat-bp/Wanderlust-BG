5539e7df8fc8a07ce0d11a030dc418c76a0415d6
bharat-bp <15bharat.patil@gmail.com>
2025-03-08 23:36:50 +0530

    BP : Terraform commit
    BP : Terraform commit

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_key_pair" "deployer" {
-  key_name   = "terra-automate-key"
-  public_key = file("/Users/shubham/Documents/work/TrainWithShubham/terra-practice/terra-key.pub")
+  key_name   = "terraform-key"
+  public_key = file("\Users\Shiva\Documents\GitHub\Wanderlust-BG\terraform")
 }
 
 resource "aws_default_vpc" "default" {
@@ -28,7 +28,7 @@ resource "aws_security_group" "allow_user_to_connect" {
   }
 
   ingress {
-    description = "port 80 allow"
+    description = "port 80 http common"
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
@@ -36,9 +36,61 @@ resource "aws_security_group" "allow_user_to_connect" {
   }
 
   ingress {
-    description = "port 443 allow"
+    description = "port 443 allow common"
     from_port   = 443
     to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "port 6379 REDIS"
+    from_port   = 6379
+    to_port     = 6379
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+    ingress {
+    description = "port 25 SMTP"
+    from_port   = 25
+    to_port     = 25
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
+  ingress {
+    description = "port 30000-32767 node-port k8s"
+    from_port   = 30000
+    to_port     = 32767
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
+  ingress {
+    description = "port 465 SMTPs MAIL"
+    from_port   = 465
+    to_port     = 465
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
+  ingress {
+    description = "port 3000-10000 kubernetes"
+    from_port   = 3000
+    to_port     = 10000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
+  ingress {
+    description = "port 6443 allow"
+    from_port   = 3000
+    to_port     = 10000
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
@@ -48,16 +100,42 @@ resource "aws_security_group" "allow_user_to_connect" {
   }
 }
 
-resource "aws_instance" "testinstance" {
-  ami             = var.ami_id
-  instance_type   = var.instance_type
+resource "aws_instance" "JK-Mater" {
+  ami             = ami-0e386fa0b67b8b12c
+  instance_type   = t3.medium
   key_name        = aws_key_pair.deployer.key_name
   security_groups = [aws_security_group.allow_user_to_connect.name]
   tags = {
-    Name = "Automate"
+    Name = "JK-Master"
   }
   root_block_device {
-    volume_size = 30 
+    volume_size = 10 
+    volume_type = "gp3"
+  }
+
+  resource "aws_instance" "JK-Agent" {
+  ami             = ami-0e386fa0b67b8b12c
+  instance_type   = t3.micro
+  key_name        = aws_key_pair.deployer.key_name
+  security_groups = [aws_security_group.allow_user_to_connect.name]
+  tags = {
+    Name = "JK-Agent"
+  }
+  root_block_device {
+    volume_size = 10 
+    volume_type = "gp3"
+  }
+
+  resource "aws_instance" "EKS-Argo" {
+  ami             = ami-0e386fa0b67b8b12c
+  instance_type   = t3.micro
+  key_name        = aws_key_pair.deployer.key_name
+  security_groups = [aws_security_group.allow_user_to_connect.name]
+  tags = {
+    Name = "EKS-Argo"
+  }
+  root_block_device {
+    volume_size = 10 
     volume_type = "gp3"
   }
 }
